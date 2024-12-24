3a21f5547117afc08c4ae73e27f49eb2540bf0ac
Maria Maltseva <mariamaltseva1106@gmail.com>
2024-12-24 12:14:43 +0300

    5

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,10 @@ STATIC_URL = 'static/'
 
 STATIC_DIRS = (BASE_DIR / 'static/')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
